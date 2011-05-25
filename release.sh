74f1c3942d54daf8c6ee15ba45d488a08bac6490
Chris McClelland <github.com@m3.ath.cx>
2011-05-25 01:05:55 +0100

    Added license and readme

--- release.sh
+++ release.sh
@@ -30,6 +30,23 @@ cp -rp gen_xsvf libfpgalink-${DATE}/
 cp -rp examples libfpgalink-${DATE}/
 rm -f libfpgalink-${DATE}/examples/c/Makefile
 
+cp -p LICENSE.txt libfpgalink-${DATE}/
+cat > libfpgalink-${DATE}/README <<EOF
+FPGALink Binary Distribution
+
+FPGALink is a library for JTAG-programming and subsequently interacting with an FPGA over USB using
+a microcontroller (primarily the Cypress FX2LP). It allows you to:
+
+   * Load and save Cypress FX2LP firmware
+   * Communicate with the FPGA using HiSpeed USB (~25Mbyte/s)
+   * Reprogram the FPGA using JTAG over USB
+   * Bootstrap an FPGA design standalone using minimal components
+
+Overview here: http://www.makestuff.eu/wordpress/?page_id=1400
+Source code here: https://github.com/makestuff/libfpgalink
+API docs here: http://www.swaton.ukfsn.org/apidocs/libfpgalink_8h.html
+EOF
+
 # Package it up
 tar zcf libfpgalink-${DATE}.tar.gz libfpgalink-${DATE}
 zip -r libfpgalink-${DATE}.zip libfpgalink-${DATE}
