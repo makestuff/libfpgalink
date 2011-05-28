6ad09aa4130be46ea0c7fddc36de878877cc988a
Chris McClelland <github.com@m3.ath.cx>
2011-05-28 19:45:09 +0100

    Added gotroom_in and switched to symbolic names for FIFO operations

--- vhdl/TopLevel.vhdl
+++ vhdl/TopLevel.vhdl
@@ -38,6 +38,7 @@ entity TopLevel is
 		-- Data & control from the FX2
 		fifodata_io  : inout std_logic_vector(7 downto 0);
 		gotdata_in   : in std_logic;                     -- FLAGC=EF (active-low), so '1' when there's data
+		gotroom_in   : in std_logic;                     -- FLAGB=FF (active-low), so '1' when there's room
 
 		-- Control to the FX2
 		sloe_out     : out std_logic;                    -- PA2
@@ -73,6 +74,10 @@ architecture Behavioural of TopLevel is
 	signal checksum, checksum_next  : std_logic_vector(15 downto 0);
 	signal r0, r1, r0_next, r1_next : std_logic_vector(7 downto 0);
 	signal r2, r3, r2_next, r3_next : std_logic_vector(7 downto 0);
+	signal fifoOp                   : std_logic_vector(2 downto 0);
+	constant FIFO_READ        : std_logic_vector(2 downto 0) := "100";  -- assert slrd_out & sloe_out
+	constant FIFO_WRITE       : std_logic_vector(2 downto 0) := "011";  -- assert slwr_out
+	constant FIFO_NOP         : std_logic_vector(2 downto 0) := "111";  -- assert nothing
 	constant OUT_FIFO               : std_logic_vector(1 downto 0) := "10"; -- EP6OUT
 	constant IN_FIFO                : std_logic_vector(1 downto 0) := "11"; -- EP8IN
 begin
@@ -102,7 +107,9 @@ begin
 	end process;
 
 	-- Next state logic
-	process(state, fifodata_io, gotdata_in, wcount, addr, r0, r1, r2, r3, checksum, dowrite, sw_in)
+	process(
+		state, fifodata_io, gotdata_in, gotroom_in, wcount, addr, r0, r1, r2, r3, checksum, dowrite,
+		sw_in)
 	begin
 		state_next    <= state;
 		wcount_next   <= wcount;
@@ -114,14 +121,13 @@ begin
 		r2_next       <= r2;
 		r3_next       <= r3;
 		fifodata_io   <= (others => 'Z');  -- Tristated unless explicitly driven
+		fifoOp        <= FIFO_READ;        -- read the FX2LP FIFO by default
+		pktend_out    <= '1';              -- inactive: FPGA does not commit a short packet.
+
 		case state is
 
 			when STATE_IDLE =>
 				fifoaddr_out <= OUT_FIFO;  -- Reading from FX2LP
-				sloe_out     <= '0';  	   -- Active: FX2LP should drive fifodata_io
-				slrd_out     <= '0';  	   -- Active: If there's data available, commit the read.
-				slwr_out     <= '1';  	   -- Inactive: FPGA does not write.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
 				if ( gotdata_in = '1' ) then
 					-- The read/write flag and a seven-bit register address will be available on the
 					-- next clock edge.
@@ -132,10 +138,6 @@ begin
 
 			when STATE_GET_COUNT0 =>
 				fifoaddr_out <= OUT_FIFO;  -- Reading from FX2LP
-				sloe_out     <= '0';  	   -- Active: FX2LP should drive fifodata_io
-				slrd_out     <= '0';  	   -- Active: If there's data available, commit the read.
-				slwr_out     <= '1';  	   -- Inactive: FPGA does not write.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
 				if ( gotdata_in = '1' ) then
 					-- The count high word high byte will be available on the next clock edge.
 					wcount_next(31 downto 24) <= unsigned(fifodata_io);
@@ -144,10 +146,6 @@ begin
 
 			when STATE_GET_COUNT1 =>
 				fifoaddr_out <= OUT_FIFO;  -- Reading from FX2LP
-				sloe_out     <= '0';  	   -- Active: FX2LP should drive fifodata_io
-				slrd_out     <= '0';  	   -- Active: If there's data available, commit the read.
-				slwr_out     <= '1';  	   -- Inactive: FPGA does not write.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
 				if ( gotdata_in = '1' ) then
 					-- The count high word low byte will be available on the next clock edge.
 					wcount_next(23 downto 16) <= unsigned(fifodata_io);
@@ -156,10 +154,6 @@ begin
 
 			when STATE_GET_COUNT2 =>
 				fifoaddr_out <= OUT_FIFO;  -- Reading from FX2LP
-				sloe_out     <= '0';  	   -- Active: FX2LP should drive fifodata_io
-				slrd_out     <= '0';  	   -- Active: If there's data available, commit the read.
-				slwr_out     <= '1';  	   -- Inactive: FPGA does not write.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
 				if ( gotdata_in = '1' ) then
 					-- The count low word high byte will be available on the next clock edge.
 					wcount_next(15 downto 8) <= unsigned(fifodata_io);
@@ -168,10 +162,6 @@ begin
 
 			when STATE_GET_COUNT3 =>
 				fifoaddr_out <= OUT_FIFO;  -- Reading from FX2LP
-				sloe_out     <= '0';  	   -- Active: FX2LP should drive fifodata_io
-				slrd_out     <= '0';  	   -- Active: If there's data available, commit the read.
-				slwr_out     <= '1';  	   -- Inactive: FPGA does not write.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
 				if ( gotdata_in = '1' ) then
 					-- The count low word low byte will be available on the next clock edge.
 					wcount_next(7 downto 0) <= unsigned(fifodata_io);
@@ -184,47 +174,39 @@ begin
 
 			when STATE_BEGIN_WRITE =>
 				fifoaddr_out <= IN_FIFO;   -- Writing to FX2LP
-				sloe_out     <= '1';  	   -- Inactive: FX2LP should not drive fifodata_io
-				slrd_out     <= '1';  	   -- Inactive: FPGA is not reading.
-				slwr_out     <= '1';  	   -- Inactive: FPGA is not writing.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
+				fifoOp       <= FIFO_NOP;
 				state_next   <= STATE_WRITE;
 
 			when STATE_WRITE =>
 				fifoaddr_out <= IN_FIFO;   -- Writing to FX2LP
-				sloe_out     <= '1';  	   -- Inactive: FX2LP should not drive fifodata_io
-				slrd_out     <= '1';  	   -- Inctive: FPGA is not reading.
-				slwr_out     <= '0';  	   -- Active: FPGA is writing.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
-				case addr is
-					when "00" =>
-						fifodata_io <= sw_in; -- Reading from R0 returns the state of the switches
-					when "01" =>
-						fifodata_io <= r1;
-					when "10" =>
-						fifodata_io <= r2;
-					when others =>
-						fifodata_io <= r3;
-				end case;
-				wcount_next  <= wcount - 1;
-				if ( wcount = 1 ) then
-					state_next <= STATE_END_WRITE;
+				if ( gotroom_in = '1' ) then
+					fifoOp <= FIFO_WRITE;
+					case addr is
+						when "00" =>
+							fifodata_io <= sw_in; -- Reading from R0 returns the state of the switches
+						when "01" =>
+							fifodata_io <= r1;
+						when "10" =>
+							fifodata_io <= r2;
+						when others =>
+							fifodata_io <= r3;
+					end case;
+					wcount_next  <= wcount - 1;
+					if ( wcount = 1 ) then
+						state_next <= STATE_END_WRITE;
+					end if;
+				else
+					fifoOp <= FIFO_NOP;
 				end if;
 
 			when STATE_END_WRITE =>
 				fifoaddr_out <= IN_FIFO;   -- Writing to FX2LP
-				sloe_out     <= '1';  	   -- Inactive: FX2LP should not drive fifodata_io
-				slrd_out     <= '1';  	   -- Inactive: FPGA is not reading.
-				slwr_out     <= '1';  	   -- Inactive: FPGA is not writing.
+				fifoOp       <= FIFO_NOP;
 				pktend_out   <= '0';  	   -- Active: FPGA commits the packet.
 				state_next   <= STATE_IDLE;
 
 			when STATE_READ =>
 				fifoaddr_out <= OUT_FIFO;  -- Reading from FX2LP
-				sloe_out     <= '0';  	   -- Active: FX2LP should drive fifodata_io
-				slrd_out     <= '0';  	   -- Active: FPGA is reading.
-				slwr_out     <= '1';  	   -- Inactive: FPGA is not writing.
-				pktend_out   <= '1';  	   -- Inactive: FPGA does not commit a short packet.
 				if ( gotdata_in = '1' ) then
 					-- A data byte will be available on the next clock edge
 					case addr is
@@ -233,7 +215,7 @@ begin
 							checksum_next <= std_logic_vector(
 								unsigned(checksum) + unsigned(fifodata_io));  -- Update R0 checksum
 						when "01" =>
-							r1_next <= fifodata_io;  -- Writing a '1' to bit 0 of R1 clears R0 checksum
+							r1_next <= fifodata_io;  -- Writing a '1' to bit 0 of R1 clears R0 cksum
 							if ( fifodata_io(0) = '1' ) then
 								checksum_next <= (others => '0');
 							end if;
@@ -249,12 +231,19 @@ begin
 				end if;
 		end case;
 	end process;
-	
+
+	-- Breakout fifoOp
+	sloe_out <= fifoOp(0);
+	slrd_out <= fifoOp(1);
+	slwr_out <= fifoOp(2);
+
 	-- Mop up all the unused inputs to prevent synthesis warnings
 	dummy_out <=
 		clkout_in and flagA_in and flagB_in and int0_in and int1_in and pa3_in and pa7_in
 		and pd_in(0) and pd_in(1) and pd_in(2) and pd_in(3)
 		and pd_in(4) and pd_in(5) and pd_in(6) and pd_in(7);
+
+
 	
 	led_out     <= r0;
 	sseg_out(7) <= '1';  -- Decimal point off
