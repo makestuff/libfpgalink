3271051c36fa47b75b1b0b9d50af1e498a15754c
makestuff <github.com@m3.ath.cx>
2012-05-30 23:34:16 +0100

    Renamed ports

--- hdl/sync/vhdl/ex_fifo/top_level.vhdl
+++ hdl/sync/vhdl/ex_fifo/top_level.vhdl
@@ -21,18 +21,22 @@ use ieee.numeric_std.all;
 
 entity top_level is
 	port(
-		-- FX2 interface
+		-- FX2 interface -----------------------------------------------------------------------------
 		fx2Clk_in     : in    std_logic;                    -- 48MHz clock from FX2
 		fx2Addr_out   : out   std_logic_vector(1 downto 0); -- select FIFO: "10" for EP6OUT, "11" for EP8IN
 		fx2Data_io    : inout std_logic_vector(7 downto 0); -- 8-bit data to/from FX2
+
+		-- When EP6OUT selected:
 		fx2Read_out   : out   std_logic;                    -- asserted (active-low) when reading from FX2
 		fx2OE_out     : out   std_logic;                    -- asserted (active-low) to tell FX2 to drive bus
 		fx2GotData_in : in    std_logic;                    -- asserted (active-high) when FX2 has data for us
+
+		-- When EP8IN selected:
 		fx2Write_out  : out   std_logic;                    -- asserted (active-low) when writing to FX2
 		fx2GotRoom_in : in    std_logic;                    -- asserted (active-high) when FX2 has room for more data from us
 		fx2PktEnd_out : out   std_logic;                    -- asserted (active-low) when a host read needs to be committed early
 
-		-- Onboard peripherals
+		-- Onboard peripherals -----------------------------------------------------------------------
 		sseg_out      : out   std_logic_vector(7 downto 0); -- seven-segment display cathodes (one for each segment)
 		anode_out     : out   std_logic_vector(3 downto 0); -- seven-segment display anodes (one for each digit)
 		led_out       : out   std_logic_vector(7 downto 0); -- eight LEDs
@@ -41,39 +45,44 @@ entity top_level is
 end top_level;
 
 architecture behavioural of top_level is
-	-- Channel read/write interface:
-	signal chanAddr                : std_logic_vector(6 downto 0);  -- the selected channel (0-127)
-	signal chanDataIn              : std_logic_vector(7 downto 0);  -- data lines used when the host reads from a channel
-	signal chanRead                : std_logic;                     -- '1' means "on the next clock rising edge, put your next byte of data on chanData_in"
-	signal chanGotData             : std_logic;                     -- channel logic can drive this low to say "I don't have data ready for you"
-	signal chanDataOut             : std_logic_vector(7 downto 0);  -- data lines used when the host writes to a channel
-	signal chanWrite               : std_logic;                     -- '1' means "on the next clock rising edge, please accept the data on chanData_out"
-	signal chanGotRoom             : std_logic;                     -- channel logic can drive this low to say "I'm not ready for more data yet"
+	-- Channel read/write interface -----------------------------------------------------------------
+	signal chanAddr  : std_logic_vector(6 downto 0);  -- the selected channel (0-127)
+
+	-- Host >> FPGA pipe:
+	signal h2fData   : std_logic_vector(7 downto 0);  -- data lines used when the host writes to a channel
+	signal h2fValid  : std_logic;                     -- '1' means "on the next clock rising edge, please accept the data on h2fData"
+	signal h2fReady  : std_logic;                     -- channel logic can drive this low to say "I'm not ready for more data yet"
+
+	-- Host << FPGA pipe:
+	signal f2hData   : std_logic_vector(7 downto 0);  -- data lines used when the host reads from a channel
+	signal f2hValid  : std_logic;                     -- channel logic can drive this low to say "I don't have data ready for you"
+	signal f2hReady  : std_logic;                     -- '1' means "on the next clock rising edge, put your next byte of data on f2hData"
+	-- ----------------------------------------------------------------------------------------------
 
 	-- Needed so that the comm_fpga module can drive both fx2Read_out and fx2OE_out
-	signal fx2Read                 : std_logic;
+	signal fx2Read           : std_logic;
 
 	-- Flags for display on the 7-seg decimal points
-	signal flags                   : std_logic_vector(3 downto 0);
+	signal flags             : std_logic_vector(3 downto 0);
 
 	-- FIFOs
-	signal fifoCount               : std_logic_vector(15 downto 0); -- MSB=writeFifo, LSB=readFifo
+	signal fifoCount         : std_logic_vector(15 downto 0); -- MSB=writeFifo, LSB=readFifo
 
 	-- Write FIFO:
-	signal writeFifoDataIn         : std_logic_vector(7 downto 0);  -- producer: data
-	signal writeFifoWrite          : std_logic;                     -- producer: write enable
-	signal writeFifoFull           : std_logic;                     -- producer: full flag
-	signal writeFifoDataOut        : std_logic_vector(7 downto 0);  -- consumer: data
-	signal writeFifoRead           : std_logic;                     -- consumer: write enable
-	signal writeFifoEmpty          : std_logic;                     -- consumer: full flag
+	signal writeFifoDataIn   : std_logic_vector(7 downto 0);  -- producer: data
+	signal writeFifoWrite    : std_logic;                     -- producer: write enable
+	signal writeFifoFull     : std_logic;                     -- producer: full flag
+	signal writeFifoDataOut  : std_logic_vector(7 downto 0);  -- consumer: data
+	signal writeFifoRead     : std_logic;                     -- consumer: write enable
+	signal writeFifoEmpty    : std_logic;                     -- consumer: empty flag
 
 	-- Read FIFO:
-	signal readFifoDataIn          : std_logic_vector(7 downto 0);  -- producer: data
-	signal readFifoWrite           : std_logic;                     -- producer: write enable
-	signal readFifoFull            : std_logic;                     -- producer: full flag
-	signal readFifoDataOut         : std_logic_vector(7 downto 0);  -- consumer: data
-	signal readFifoRead            : std_logic;                     -- consumer: write enable
-	signal readFifoEmpty           : std_logic;                     -- consumer: full flag
+	signal readFifoDataIn    : std_logic_vector(7 downto 0);  -- producer: data
+	signal readFifoWrite     : std_logic;                     -- producer: write enable
+	signal readFifoFull      : std_logic;                     -- producer: full flag
+	signal readFifoDataOut   : std_logic_vector(7 downto 0);  -- consumer: data
+	signal readFifoRead      : std_logic;                     -- consumer: write enable
+	signal readFifoEmpty     : std_logic;                     -- consumer: empty flag
 
 	-- Counter which endlessly puts items in read FIFO for the host to read
 	signal count, count_next : std_logic_vector(7 downto 0) := (others => '0');
@@ -87,20 +96,20 @@ begin
 	end process;
 
 	-- Wire up write FIFO to channel 0:
-	writeFifoDataIn <= chanDataOut;
+	writeFifoDataIn <= h2fData;
 	writeFifoWrite <=
-		'1' when chanWrite = '1' and chanAddr = "0000000"
+		'1' when h2fValid = '1' and chanAddr = "0000000"
 		else '0';
-	chanGotRoom <=
+	h2fReady <=
 		'0' when writeFifoFull = '1' and chanAddr = "0000000"
 		else '1';
 
 	-- Wire up read FIFO to channel 0:
 	readFifoDataIn <= count;
 	readFifoRead <=
-		'1' when chanRead = '1' and chanAddr = "0000000"
+		'1' when f2hReady = '1' and chanAddr = "0000000"
 		else '0';
-	chanGotData <=
+	f2hValid <=
 		'0' when readFifoEmpty = '1' and chanAddr = "0000000"
 		else '1';
 	count_next <=
@@ -108,7 +117,7 @@ begin
 		else count;
 	
 	-- Select values to return for each channel when the host is reading
-	with chanAddr select chanDataIn <=
+	with chanAddr select f2hData <=
 		readFifoDataOut        when "0000000",  -- get data from the read FIFO
 		fifoCount(15 downto 8) when "0000001",  -- read the current depth of the write FIFO
 		fifoCount(7 downto 0)  when "0000010",  -- read the current depth of the write FIFO
@@ -132,12 +141,12 @@ begin
 
 			-- Channel read/write interface
 			chanAddr_out   => chanAddr,
-			chanData_in    => chanDataIn,
-			chanRead_out   => chanRead,
-			chanGotData_in => chanGotData,
-			chanData_out   => chanDataOut,
-			chanWrite_out  => chanWrite,
-			chanGotRoom_in => chanGotRoom
+			f2hData_in    => f2hData,
+			f2hReady_out   => f2hReady,
+			f2hValid_in => f2hValid,
+			h2fData_out   => h2fData,
+			h2fValid_out  => h2fValid,
+			h2fReady_in => h2fReady
 		);
 
 	-- Write FIFO: written by host, read by leds
