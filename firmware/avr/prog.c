9dd8fb57b9166a7d335dc1ea000f0346b531ae06
makestuff <github.com@m3.ath.cx>
2013-07-19 19:44:47 +0100

    prog: Use hardware SPI for JTAG (if possible)

--- firmware/avr/prog.c
+++ firmware/avr/prog.c
@@ -24,15 +24,24 @@ static ProgOp m_progOp = PROG_NOP;
 static uint8 m_flagByte = 0x00;
 
 // Port and bits to use:
-#define PROG_PORT B
-#define bmTMS     0x01
-#define bmTCK     0x02
-#define bmTDI     0x04
-#define bmTDO     0x08
+#define JTAG_HWSPI
+#ifdef JTAG_HWSPI
+	// These are fixed for a given micro: the SPI pins
+	#define PROG_PORT B
+	#define bmTMS     0x01
+	#define bmTCK     0x02
+	#define bmTDI     0x04
+	#define bmTDO     0x08
+#else
+	// These can be anything, depending on how JTAG is wired
+	#define PROG_PORT B
+	#define bmTMS     0x01
+	#define bmTCK     0x02
+	#define bmTDI     0x04
+	#define bmTDO     0x08
+#endif
 #define bmSCLK    bmTDI
 #define bmMOSI    bmTCK
-//#define bmSCLK    0x10
-//#define bmMOSI    0x20
 
 // Make PORT{B,C,D...} & PIN{B,C,D...} macros
 #define PORT CONCAT(PORT, PROG_PORT)
@@ -59,12 +68,85 @@ void progClockFSM(uint32 bitPattern, uint8 transitionCount) {
 #define jtagBit(x) setTDI(byte & x); PORT |= bmTCK; PORT &= ~bmTCK
 #define mosiBit(x) setMOSI(byte & x); PORT |= bmSCLK; PORT &= ~bmSCLK
 
-// TCK-clock the supplied byte into TDI, LSB first.
-//
-static void jtagShiftOut(uint8 byte) {
-	jtagBit(0x01); jtagBit(0x02); jtagBit(0x04); jtagBit(0x08);
-	jtagBit(0x10); jtagBit(0x20); jtagBit(0x40); jtagBit(0x80);
-}
+// If the JTAG lines are wired to the right pins on the AVR SPI port, we can use
+// it instead of bit-banging, which is much faster
+//#if (PROG_PORT == B) && (bmTDO == 0x08) && (bmTDI == 0x04) && (bmTMS == 0x01) && (bmTMS == 0x02)
+#ifdef JTAG_HWSPI
+	// Use SPI hardware!
+
+	// TCK-clock the supplied byte into TDI, LSB first.
+	static inline void jtagShiftOut(uint8 byte) {
+		SPDR = byte;
+		while ( !(SPSR & (1<<SPIF)) );
+	}
+
+	// JTAG-clock the supplied byte into TDI, MSB first. Return the byte clocked out of TDO.
+	static inline uint8 jtagShiftInOut(uint8 byte) {
+		SPDR = byte;
+		while ( !(SPSR & (1<<SPIF)) );
+		return SPDR;
+	}
+
+	// Enable the SPI port
+	static inline void spiEnable(void) {
+		SPSR = (1<<SPI2X);
+		SPCR = (1<<SPE) | (1<<DORD) | (1<<MSTR) | (0<<SPR0);
+	}
+
+	// Disable the SPI port
+	static inline void spiDisable(void) {
+		SPSR = 0x00;
+		SPCR = 0x00;
+	}
+#else
+	// Use bit-banging!
+
+	// TCK-clock the supplied byte into TDI, LSB first
+	static void jtagShiftOut(uint8 byte) {
+		jtagBit(0x01); jtagBit(0x02); jtagBit(0x04); jtagBit(0x08);
+		jtagBit(0x10); jtagBit(0x20); jtagBit(0x40); jtagBit(0x80);
+	}
+
+	// JTAG-clock the supplied byte into TDI, MSB first. Return the byte clocked out of TDO.
+	static uint8 jtagShiftInOut(uint8 byte) {
+		uint8 tdoByte = 0x00;
+		setTDI(byte & 0x01);
+		if ( PIN & bmTDO ) { tdoByte |= 0x01; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x02);
+		if ( PIN & bmTDO ) { tdoByte |= 0x02; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x04);
+		if ( PIN & bmTDO ) { tdoByte |= 0x04; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x08);
+		if ( PIN & bmTDO ) { tdoByte |= 0x08; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x10);
+		if ( PIN & bmTDO ) { tdoByte |= 0x10; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x20);
+		if ( PIN & bmTDO ) { tdoByte |= 0x20; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x40);
+		if ( PIN & bmTDO ) { tdoByte |= 0x40; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		setTDI(byte & 0x80);
+		if ( PIN & bmTDO ) { tdoByte |= 0x80; }
+		PORT |= bmTCK; PORT &= ~bmTCK;
+		return tdoByte;
+	}
+
+	// Enable the SPI port
+	static inline void spiEnable(void) {
+		// We're bit-banging, so nothing needs to be done
+	}
+
+	// Disable the SPI port
+	static inline void spiDisable(void) {
+		// We're bit-banging, so nothing needs to be done
+	}
+#endif
 
 // SCLK-clock the supplied byte into MOSI, LSB first.
 //
@@ -76,37 +158,6 @@ static void jtagShiftOut(uint8 byte) {
 		mosiBit(0x10); mosiBit(0x20); mosiBit(0x40); mosiBit(0x80);
 	}
 #endif
-
-// JTAG-clock the supplied byte into TDI, MSB first. Return the byte clocked out of TDO.
-//
-static uint8 shiftInOut(uint8 byte) {
-	uint8 tdoByte = 0x00;
-	setTDI(byte & 0x01);
-	if ( PIN & bmTDO ) { tdoByte |= 0x01; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x02);
-	if ( PIN & bmTDO ) { tdoByte |= 0x02; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x04);
-	if ( PIN & bmTDO ) { tdoByte |= 0x04; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x08);
-	if ( PIN & bmTDO ) { tdoByte |= 0x08; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x10);
-	if ( PIN & bmTDO ) { tdoByte |= 0x10; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x20);
-	if ( PIN & bmTDO ) { tdoByte |= 0x20; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x40);
-	if ( PIN & bmTDO ) { tdoByte |= 0x40; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	setTDI(byte & 0x80);
-	if ( PIN & bmTDO ) { tdoByte |= 0x80; }
-	PORT |= bmTCK; PORT &= ~bmTCK;
-	return tdoByte;
-}
 
 // Kick off a shift operation. Next time progExecuteShift() runs, it will execute the shift.
 //
@@ -125,6 +176,7 @@ static void jtagIsSendingIsReceiving(void) {
 	uint16 bitsRead, bitsRemaining;
 	uint8 bytesRead, bytesRemaining;
 	uint8 buf[ENDPOINT_SIZE], *ptr;
+	spiEnable();
 	while ( m_numBits ) {
 		bitsRead = (m_numBits >= (ENDPOINT_SIZE<<3)) ? ENDPOINT_SIZE<<3 : m_numBits;
 		bytesRead = bitsToBytes(bitsRead);
@@ -138,9 +190,10 @@ static void jtagIsSendingIsReceiving(void) {
 			leftOver = (uint8)(bitsRead - bitsRemaining); // How many bits in last byte (1-8)
 			bytesRemaining = (bitsRemaining>>3);
 			while ( bytesRemaining-- ) {
-				*ptr = shiftInOut(*ptr);
+				*ptr = jtagShiftInOut(*ptr);
 				ptr++;
 			}
+			spiDisable();
 			tdiByte = *ptr;  // Now do the bits in the final byte
 			tdoByte = 0x00;
 			i = 1;
@@ -163,7 +216,7 @@ static void jtagIsSendingIsReceiving(void) {
 			// This is not the last chunk
 			bytesRemaining = (bitsRead>>3);
 			while ( bytesRemaining-- ) {
-				*ptr = shiftInOut(*ptr);
+				*ptr = jtagShiftInOut(*ptr);
 				ptr++;
 			}
 		}
@@ -182,6 +235,7 @@ static void jtagIsSendingNotReceiving(void) {
 	uint8 bytesRead, bytesRemaining;
 	uint8 buf[ENDPOINT_SIZE], *ptr;
 	Endpoint_SelectEndpoint(OUT_ENDPOINT_ADDR);
+	spiEnable();
 	while ( m_numBits ) {
 		bitsRead = (m_numBits >= (ENDPOINT_SIZE<<3)) ? ENDPOINT_SIZE<<3 : m_numBits;
 		bytesRead = bitsToBytes(bitsRead);
@@ -196,8 +250,8 @@ static void jtagIsSendingNotReceiving(void) {
 			while ( bytesRemaining-- ) {
 				jtagShiftOut(*ptr++);
 			}
+			spiDisable();
 			tdiByte = *ptr;  // Now do the bits in the final byte
-			
 			i = 1;
 			while ( i && leftOver ) {
 				leftOver--;
@@ -230,6 +284,7 @@ static void jtagNotSendingIsReceiving(void) {
 	uint8 buf[ENDPOINT_SIZE], *ptr;
 	const uint8 tdiByte = (m_flagByte & bmSENDONES) ? 0xFF : 0x00;
 	Endpoint_SelectEndpoint(IN_ENDPOINT_ADDR);
+	spiEnable();
 	while ( m_numBits ) {
 		bitsRead = (m_numBits >= (ENDPOINT_SIZE<<3)) ? ENDPOINT_SIZE<<3 : m_numBits;
 		bytesRead = bitsToBytes(bitsRead);
@@ -241,9 +296,10 @@ static void jtagNotSendingIsReceiving(void) {
 			leftOver = (uint8)(bitsRead - bitsRemaining); // How many bits in last byte (1-8)
 			bytesRemaining = (bitsRemaining>>3);
 			while ( bytesRemaining-- ) {
-				*ptr++ = shiftInOut(tdiByte);
+				*ptr++ = jtagShiftInOut(tdiByte);
 			}
-			tdoByte = 0x00;
+			spiDisable();
+			tdoByte = 0x00;  // Now do the bits in the final byte
 			i = 1;
 			setTDI(tdiByte & 0x01);
 			while ( i && leftOver ) {
@@ -263,7 +319,7 @@ static void jtagNotSendingIsReceiving(void) {
 			// This is not the last chunk
 			bytesRemaining = (bitsRead>>3);
 			while ( bytesRemaining-- ) {
-				*ptr++ = shiftInOut(tdiByte);
+				*ptr++ = jtagShiftInOut(tdiByte);
 			}
 		}
 		Endpoint_Write_Stream_LE(buf, bytesRead, NULL);
@@ -281,9 +337,11 @@ static void jtagNotSendingNotReceiving(void) {
 	bitsRemaining = (m_numBits-1) & 0xFFFFFFF8;    // Now an integer number of bytes
 	leftOver = (uint8)(m_numBits - bitsRemaining); // How many bits in last byte (1-8)
 	bytesRemaining = (bitsRemaining>>3);
+	spiEnable();
 	while ( bytesRemaining-- ) {
 		jtagShiftOut(tdiByte);
 	}
+	spiDisable();
 	setTDI(tdiByte & 0x01);
 	while ( leftOver ) {
 		leftOver--;
